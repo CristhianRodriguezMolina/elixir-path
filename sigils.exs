02aeafb8a01b2ccc9a6668795ef7479bbc5ab4c8
CristhianRodriguezMolina <camilo.r13@hotmail.com>
2021-11-23 08:16:31 -0500

    Deleting duplicated files

--- sigils.exs
+++
@@ -1,6 +0,0 @@
-defmodule MySigils do
-  def sigil_d(string, []), do: String.to_integer(string)
-  def sigil_d(string, [?d]), do: String.to_integer(string) * 2
-  def sigil_d(string, [?t]), do: String.to_integer(string) * 3
-  def sigil_d(string, [?c]), do: String.to_integer(string) * 4
-end
